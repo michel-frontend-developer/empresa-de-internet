95c2032e2365e1c67a9a47a33d800e9557045dc4
michel <michel.desenvolvedorweb@gmail.com>
2020-12-22 22:17:14 -0300

    update

--- javascript/index.js
+++ javascript/index.js
@@ -4,36 +4,56 @@ window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', window.scrollY > 0 && window.innerWidth > 600);
 });
 
-//  modal
+//  modal and form
 const contact = document.querySelectorAll('.sign-it');
 const modal = document.querySelector('.modal-container');
+const modalContent = document.querySelector('.modal-content');
 const send = document.querySelector('.btn-send');
 const inputName = document.getElementById('name');
 const inputPhone = document.getElementById('phone');
 const inputCEP = document.getElementById('cep');
 const inputNumber = document.getElementById('number');
+const modalSucess = document.querySelector('.modal-sucess');
+const modalSended = document.querySelector('.modal-sended');
 
 //  show modal
 contact.forEach(function(e){
     e.addEventListener('click', function(element){
         element.preventDefault();
-        modal.classList.add('show');
+        gsap.to(modal, {duration: 0, autoAlpha: 1});
+        gsap.from(modalContent, {duration: .3, autoAlpha: 0, y: -60});
     });
 });
 
 //  close modal
 modal.addEventListener('click', function(e){
     if(e.target.classList.contains('modal-container') || e.target.classList.contains('far')){
-        modal.classList.remove('show');
+        gsap.to(modal, {duration: 0, autoAlpha: 0});
     }
 });
 
-//  send
+//  validation
 send.addEventListener('click', function(e){
     e.preventDefault();
-    modal.classList.remove('show');
-    inputName.value = '';
-    inputPhone.value = '';
-    inputCEP.value = '';
-    inputNumber.value = '';
+    if(inputName.value === '' || inputPhone.value === '' || inputCEP.value === '' || inputNumber.value === ''){
+        alert('preencha todos os campos!');
+    }else{
+        sendedAnimation();
+    }
 });
+
+//  sended sucess
+function sendedAnimation(){
+    gsap.to(modal, {duration: .3, autoAlpha: 0});
+    gsap.to(modalSucess, {duration: .3, autoAlpha: 1});
+    gsap.from(modalSended, {duration: .3, y: 60});
+
+    setTimeout(function(){
+        gsap.to(modalSucess, {duration: .3, autoAlpha: 0});
+
+        inputName.value = '';
+        inputPhone.value = '';
+        inputCEP.value = '';
+        inputNumber.value = '';
+    }, 2000);
+}
